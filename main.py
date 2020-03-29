56a8dffe03bfd69260ac3a9970cf0a3fa04c48a9
Ania <41090467+aniaskudlarska@users.noreply.github.com>
2020-03-29 11:58:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import os
 
 owned_sets = ['core', 'dwl', 'ptc', 'tcu']
 rootDir = '.\card-jsons'
-for dirName, subdirList, fileList in os.walk(rootDir, topdown=False):
-    print('Found directory: %s' % dirName)
-    for fname in fileList:
-        if 'encounter' not in fname: #traverse non encounter files
-            print('\t%s' % fname)
+
+for dirName, subdirList, fileList in os.walk(rootDir):
+    for file in fileList:
+        print(file)
