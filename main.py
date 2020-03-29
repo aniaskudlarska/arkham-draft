891c735f4dd62c42f9b7190cfffd522ecf81ff6e
Ania <41090467+aniaskudlarska@users.noreply.github.com>
2020-03-29 13:01:15 -0400

    Made loader in main
    Fixed stella's file for formatting cus I'm lazy

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import json
 import os
 
+
 owned_sets = ['core', 'dwl', 'ptc', 'tcu']
 rootDir = '.\card-jsons'
-
-for dirName, subdirList, fileList in os.walk(rootDir):
+dictList = []
+for dirName, subdirList, fileList in os.walk(rootDir, topdown=True):
     for file in fileList:
-        print(file)
+        if 'encounter' not in file:
+                if file.endswith('.json'):
+                    fname = os.path.join(dirName,file)
+                    with open (fname, encoding="utf8") as myfile:
+                        print(fname)
+                        distros_dict = json.load(myfile)
+                        dictList.append(distros_dict)
+                  
+
+print(len(dictList))
