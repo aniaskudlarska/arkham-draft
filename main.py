0bd7497ff57a4bf0d81e93d95b7a7ad497de39a3
Ania <41090467+aniaskudlarska@users.noreply.github.com>
2021-04-20 22:43:09 -0400

    Todos, next steps

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
 import os
+import random
 
+#TODO: Parsers! lots of parsers oh god
 
 owned_sets = ['core', 'dwl', 'ptc', 'tcu']
+
 rootDir = '.\card-jsons'
 dictList = []
 for dirName, subdirList, fileList in os.walk(rootDir, topdown=True):
@@ -11,9 +14,27 @@ for dirName, subdirList, fileList in os.walk(rootDir, topdown=True):
                 if file.endswith('.json'):
                     fname = os.path.join(dirName,file)
                     with open (fname, encoding="utf8") as myfile:
-                        print(fname)
                         distros_dict = json.load(myfile)
                         dictList.append(distros_dict)
-                  
 
-print(len(dictList))
+
+#print(len(dictList))
+
+namelist = []
+for set in dictList:
+   for card in set:
+        if 'traits' in card.keys():
+            if "Spirit." in card['traits']:
+                #print(card['name'])
+                pass
+
+#print(dictList)
+n = random.choice(dictList)
+print(n[0]['name'])
+nslice = ()
+decklist = []
+
+#build cardlist
+
+
+
